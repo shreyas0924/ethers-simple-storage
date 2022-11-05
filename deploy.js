acd1646454ec2862ee43e85d13420e13585f421b
shreyas0924 <shreyasp0924@gmail.com>
2022-11-05 13:44:40 +0530

    private key gaanche

--- deploy.js
+++ deploy.js
@@ -2,7 +2,10 @@ const ethers = require("ethers")
 
 async function main() {
   const provdier = new ethers.providers.JsonRpcProvider("http://127.0.0.1:7545") // provider ganache
-  const wallet = new ethers.Wallet("PRIVATE_KEY", provdier)
+  const wallet = new ethers.Wallet(
+    "7861f938d4a6d3c14b2f4ad57096ae7ce22e5106c9d82bda2b85dda31fee65b6",
+    provdier
+  )
 }
 
 main()
