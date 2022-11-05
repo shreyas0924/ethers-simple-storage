519608189d28d84ef2bf71742190c017b47b20b9
shreyas0924 <shreyasp0924@gmail.com>
2022-11-05 14:01:02 +0530

    deployed on ganache

--- deploy.js
+++ deploy.js
@@ -1,11 +1,22 @@
 const ethers = require("ethers")
+const fs = require("fs-extra")
 
 async function main() {
-  const provdier = new ethers.providers.JsonRpcProvider("http://127.0.0.1:7545") // provider ganache
+  const provdier = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545") // provider ganache
   const wallet = new ethers.Wallet(
-    "7861f938d4a6d3c14b2f4ad57096ae7ce22e5106c9d82bda2b85dda31fee65b6",
+    "0x05b1b978cdbc8fbc9c48e16dd7f7317ee07193ffcb0eb53705bbf555cf8ab8b8", //ganache private key
     provdier
   )
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf-8")
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf-8"
+  )
+
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet)
+  console.log("Deploying........")
+  const contract = await contractFactory.deploy()
+  console.log(contract)
 }
 
 main()
